ceda7a8a44c602718094e69703e82c55f011b727
blackopplays <131572176+SPYBLACK1@users.noreply.github.com>
2025-07-25 00:35:25 +0530

    Update botinfo.js

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -37,7 +37,7 @@ module.exports = {
       .addFields(
         {
           name: 'Developer',
-          value: '```elm\nGaurav & Neppixel\n```',
+          value: '```elm\nBLACKOP\n```',
           inline: true,
         },
         {
